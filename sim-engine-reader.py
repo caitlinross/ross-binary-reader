23a9f0c351749a9c3b0607e77f7307298c417573
Caitlin Ross <rossc3@rpi.edu>
2019-03-10 13:18:01 -0400

    supporting other network models

--- sim-engine-reader.py
+++ sim-engine-reader.py
@@ -5,11 +5,13 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--filename", required=True, help="binary file to convert")
 ap.add_argument("-m", "--machine_map", required=False, help="a csv of machine mapping data (csv: MPI rank,node id, socket id, core id)")
+ap.add_argument("-n", "--network", required=False, help="if file contains model data, select dragonfly, slimfly, or fattree")
 ap.add_argument("-r", "--radix", required=False, help="if file contains model data, the radix of the network model")
 args = vars(ap.parse_args())
 
 filename = args["filename"]
 stem = filename.split(".")[0]
+network = args["network"]
 
 mmap = {}
 
@@ -50,7 +52,7 @@ def setup_lp_output():
     lp_out.write("PE,KP,LP,VT,RT,event_proc,ev_abort,ev_rb,nsend_net,nrecv_net\n")
     return lp_out
 
-def setup_model_output():
+def setup_dragonfly_model_output():
     radix = int(args["radix"])
     router_out = open(stem + "-router-output.csv", "w")
     terminal_out = open(stem + "-terminal_output.csv", "w")
@@ -64,6 +66,20 @@ def setup_model_output():
 
     return router_out, terminal_out
 
+def setup_fattree_model_output():
+    radix = int(args["radix"])
+    router_out = open(stem + "-switch-output.csv", "w")
+    terminal_out = open(stem + "-terminal_output.csv", "w")
+    terminal_out.write("PE,KP,LP,end_time,terminal_id,vc_occupancy\n")
+    router_out.write("PE,KP,LP,end_time,router_id")
+    for i in range(radix):
+        router_out.write(",vc_occupancy_" + str(i))
+    router_out.write("\n")
+
+    return router_out, terminal_out
+
+model_name_size = 16
+
 metadata_sz = 48
 last_gvt = 0
 vts = 1
@@ -75,11 +91,13 @@ has_lp = 6
 has_model = 7
 num_model_lps = 8
 
-m_md_sz = 12
-m_kpid = 0
-m_lpid = 1
-m_num_vars = 2
+m_md_sz = 28
+m_name = 0
+m_kpid = 16
+m_lpid = 17
+m_num_vars = 18
 
+var_md_struct_str = "@3i"
 mv_md_sz = 12
 mv_id = 0
 mv_type = 1
@@ -119,6 +137,42 @@ def model_var_type_char(type):
         return "d"
 
     return ""
+
+def router_write(pos):
+    router_out.write(str(metadata[peid]) + "," + str(model_md[m_kpid]) + "," + str(model_md[m_lpid]) + ",")
+    router_out.write(str(metadata[vt]))
+    #router_out.write(str(metadata[rts]))
+    for i in range(model_md[m_num_vars]):
+        router_out.write(",")
+        var_md = struct.unpack(var_md_struct_str, binary_file.read(mv_md_sz))
+        pos += mv_md_sz
+        binary_file.seek(pos)
+        var_data_size = model_var_type_size(var_md[mv_type]) * var_md[mv_num_elems]
+        data_str = "@" + str(var_md[mv_num_elems]) + model_var_type_char(var_md[mv_type])
+        var_data = struct.unpack(data_str, binary_file.read(var_data_size))
+        pos += var_data_size
+        binary_file.seek(pos)
+        router_out.write(','.join(str(p) for p in var_data))
+    router_out.write("\n")
+    return pos
+
+def terminal_write(pos):
+    terminal_out.write(str(metadata[peid]) + "," + str(model_md[m_kpid]) + "," + str(model_md[m_lpid]) + ",")
+    terminal_out.write(str(metadata[vt]))
+    #terminal_out.write(str(metadata[rts]))
+    for i in range(model_md[m_num_vars]):
+        terminal_out.write(",")
+        var_md = struct.unpack(var_md_struct_str, binary_file.read(mv_md_sz))
+        pos += mv_md_sz
+        binary_file.seek(pos)
+        var_data_size = model_var_type_size(var_md[mv_type]) * var_md[mv_num_elems]
+        data_str = "@" + str(var_md[mv_num_elems]) + model_var_type_char(var_md[mv_type])
+        var_data = struct.unpack(data_str, binary_file.read(var_data_size))
+        pos += var_data_size
+        binary_file.seek(pos)
+        terminal_out.write(','.join(str(p) for p in var_data))
+    terminal_out.write("\n")
+    return pos
 
 with open(filename, "rb") as binary_file:
     binary_file.seek(0, 2)
@@ -218,54 +272,24 @@ with open(filename, "rb") as binary_file:
 
         if metadata[has_model] == 1:
             if router_out is None or terminal_out is None:
-                router_out, terminal_out = setup_model_output()
+                if (network == "dragonfly"):
+                    router_out, terminal_out = setup_dragonfly_model_output()
+                elif (network == "fattree" or network == "slimfly"):
+                    router_out, terminal_out = setup_fattree_model_output()
 
             for mlp in range(metadata[num_model_lps]):
                 # first get model metadata
-                struct_str = "@IIi"
+                struct_str = "@16cIIi"
                 model_md = struct.unpack(struct_str, binary_file.read(m_md_sz))
                 pos += m_md_sz
                 binary_file.seek(pos)
                 #print("model_md: ")
                 #print(model_md)
+                mlp_name = ''.join(model_md[m_name:model_name_size])
+                if "term" in mlp_name:
+                    pos = terminal_write(pos)
+                if "rout" in mlp_name or "swit" in mlp_name:
+                    pos = router_write(pos)
 
-                struct_str = "@3i"
-                if model_md[m_num_vars] == 3: # router
-                    router_out.write(str(metadata[peid]) + "," + str(model_md[m_kpid]) + "," + str(model_md[m_lpid]) + ",")
-                    router_out.write(str(metadata[vt]))
-                    #router_out.write(str(metadata[rts]))
-                    for i in range(model_md[m_num_vars]):
-                        router_out.write(",")
-                        var_md = struct.unpack(struct_str, binary_file.read(mv_md_sz))
-                        pos += mv_md_sz
-                        binary_file.seek(pos)
-                        var_data_size = model_var_type_size(var_md[mv_type]) * var_md[mv_num_elems]
-                        data_str = "@" + str(var_md[mv_num_elems]) + model_var_type_char(var_md[mv_type])
-                        var_data = struct.unpack(data_str, binary_file.read(var_data_size))
-                        pos += var_data_size
-                        binary_file.seek(pos)
-                        router_out.write(','.join(str(p) for p in var_data))
-                    router_out.write("\n")
-
-                elif model_md[m_num_vars] == 6: #terminal
-                    terminal_out.write(str(metadata[peid]) + "," + str(model_md[m_kpid]) + "," + str(model_md[m_lpid]) + ",")
-                    terminal_out.write(str(metadata[vt]))
-                    #terminal_out.write(str(metadata[rts]))
-                    for i in range(model_md[m_num_vars]):
-                        terminal_out.write(",")
-                        var_md = struct.unpack(struct_str, binary_file.read(mv_md_sz))
-                        pos += mv_md_sz
-                        binary_file.seek(pos)
-                        var_data_size = model_var_type_size(var_md[mv_type]) * var_md[mv_num_elems]
-                        data_str = "@" + str(var_md[mv_num_elems]) + model_var_type_char(var_md[mv_type])
-                        var_data = struct.unpack(data_str, binary_file.read(var_data_size))
-                        pos += var_data_size
-                        binary_file.seek(pos)
-                        terminal_out.write(','.join(str(p) for p in var_data))
-                    terminal_out.write("\n")
-
-                else:
-                    sys.exit("Number of model variables incorrect!")
-            
 
     print("position == " + str(pos))
