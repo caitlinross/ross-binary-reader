81a1cfcde4c1a2428e10ddaa6713d8469190c37f
Caitlin Ross <rossc3@rpi.edu>
2019-07-06 17:45:30 -0400

    updates from reducing data size for lvt sampling

--- lvt-sample-reader.py
+++ lvt-sample-reader.py
@@ -11,28 +11,20 @@ stem = filename.split(".")[0]
 
 def setup_kp_output():
     kp_out = open(stem + "-kp-output.csv", "w")
-    kp_out.write("PE,KP,GVT,RT,LVT\n")
+    kp_out.write("PE,KP,GVT,LVT\n")
     return kp_out
 
 ### Struct info for ROSS sample_metadata struct
-metadata_sz = 56
-last_gvt = 0
-vts = 1
-rts = 2
-peid = 3
-kp_gid = 4
-has_pe = 5
-has_kp = 6
-has_lp = 7
-has_model = 8
-num_model_lps = 9
-md_struct_str = "@3dI7i"
+metadata_sz = 16
+peid = 0
+has_kp = 1
+last_gvt = 2
+md_struct_str = "@2Id"
 
 ### Info for ROSS st_kp_lvt_samp struct
-kp_struct_sz = 16
-kpid = 0
-lvt = 1
-kp_struct_str = "@Id"
+kp_struct_sz = 8
+lvt = 0
+kp_struct_str = "@d"
 
 kp_out = None
 
@@ -46,16 +38,9 @@ with open(filename, "rb") as binary_file:
     file_md_sz = 12
     file_md = struct.unpack("@IIi", binary_file.read(file_md_sz))
     npe = file_md[0];
-    print(npe)
+    print(str(npe) + " PEs")
     nkp = file_md[1];
-    print(nkp)
-    inst_mode = file_md[2]
-    print(inst_mode)
-    vt = last_gvt
-    if inst_mode == 2: # VTS
-        vt = vts
-    #nlp_list = struct.unpack("@" + str(npe) + "Q", binary_file.read(npe * 8))
-    #print(nlp_list)
+    print(str(nkp) + " KPs")
 
     pos = file_md_sz
     while (pos < num_bytes):
@@ -75,9 +60,8 @@ with open(filename, "rb") as binary_file:
                 pos += kp_struct_sz
                 binary_file.seek(pos)
 
-                kp_out.write(str(metadata[peid]) + "," + str(data[kpid]) + ",")
-                kp_out.write(str(metadata[vt]) + ",")
-                kp_out.write(str(metadata[rts]) + ",")
+                kp_out.write(str(metadata[peid]) + "," + str(i) + ",")
+                kp_out.write(str(metadata[last_gvt]) + ",")
                 kp_out.write(str(data[lvt]))
                 kp_out.write("\n")
 
